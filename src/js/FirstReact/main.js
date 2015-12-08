e9be2a48cb69b376e31101d06c84a2f7d7c58cf2
Stijn <cleerenstijn@gmail.com>
2015-12-08 19:50:17 +0100

    ReactFirst and Jsx demo example

--- src/js/FirstReact/main.js
+++ src/js/FirstReact/main.js
@@ -3,8 +3,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var header1 = React.DOM.h1(null, "Tralala 1");
-var header2 = React.DOM.h1(null, "Tralala 3");
-var myDiv = React.DOM.div(null, [header1, header2]);
+var title1 = React.DOM.h3({className: 'title' }, "CC Web - React & Flux");
+var p1 = React.DOM.p(null, "React is the most popular Facebook Javascript library ....");
+
+var div1 = React.DOM.div({className:"ccDiv"}, [title1, p1]);
+
+
+var title2 = React.DOM.h3({ className: 'title' }, "CC IoT - Internet of Things @ M$");
+var p2 = React.DOM.p(null, "The Internet of Things does not need to be complicated ...");
+
+var div2 = React.DOM.div({className:"ccDiv"}, [title2, p2]);
+
+
+var header1 = React.DOM.h1(null, "CC microsoft meeting #10");
+
+var myDiv = React.DOM.div({id:'mainDiv'}, [header1, div1, div2]);
 
 ReactDOM.render(myDiv, document.getElementById('react-container'));
--- src/js/Jsx/main.jsx
+++ src/js/Jsx/main.jsx
@@ -1,1 +1,17 @@
-<div><h1>Tralalala</h1></div>
+'use strict';
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+ReactDOM.render(<div id='mainDiv'>
+					<h1 >CC microsoft meeting #10</h1>
+					<div className="ccDiv">
+						<h3 className='title'>CC Web - React &amp; Flux</h3>
+						<p>React is the most popular Facebook Javascript library ....</p>
+					</div>
+					<div className="ccDiv">
+						<h3 className= 'title'>CC IoT - Internet of Things @ M$</h3>
+						<p>The Internet of Things does not need to be complicated ...</p>
+					</div>					
+				</div>, 
+				document.getElementById('react-container'));
